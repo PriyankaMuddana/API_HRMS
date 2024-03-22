c13cf00bdcac830d3742c251a1559a9f4857c9e0
Priyanka Muddana <priyankamuddana.it@gmail.com>
2024-03-22 16:51:19 +0530

    Added serilalizer

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,3 +1,3 @@
 class EmployeeSerializer < ActiveModel::Serializer
-  	attributes :id, :first_name, :last_name, :yearly_salary, :tax_amount, :cess_amount
+	attributes :id, :first_name, :last_name, :yearly_salary, :tax_amount, :cess_amount
 end
